784620679150d16c46af5a88fe627bb225b3154c
keriarmstrong <khetto@live.com>
2023-01-26 09:55:53 -0500

    change bg color on type vale

--- grid-Template.js
+++ grid-Template.js
@@ -40,8 +40,33 @@ let searchValue = search.value
         pokemonDetails = document.querySelector('.details')
 
         pokemonImg.innerHTML = `<img class="img" src=${art} />`
-        pokemonDetails.innerHTML = `<h3>${pokemon.name.charAt(0).toUpperCase()+pokemon.name.slice(1)}</h3><ul><li>Index# ${pokedexIndex}</li><li>Height: ${heightRaw}</li><li>Weight: ${weight}</li><li>Type: ${type1}</ul>`
+        pokemonDetails.innerHTML = `<h3>${pokemon.name.charAt(0).toUpperCase()+pokemon.name.slice(1)}</h3><ul><li>Index# ${pokedexIndex}</li><li>Height: ${heightRaw}</li><li>Weight: ${weight}</li><li class="type" >Type: ${type1}</ul>`
         // main.append(pokemonImg, pokemonDetails)
+
+        let type = document.getElementsByClassName('type')
+        let selectedType = (type[0].innerHTML)
+        console.log(selectedType)
+
+        if(selectedType.includes('fire')){
+            pokemonImg.setAttribute('style', "background-color: red;")
+        }else if(selectedType.includes('grass')){
+            pokemonImg.setAttribute('style', "background-color: green;")
+        }else if(selectedType.includes('psychic')){
+            pokemonImg.setAttribute('style', "background-color: purple;")
+        }else if(selectedType.includes('water')){
+            pokemonImg.setAttribute('style', "background-color: blue;")
+        }else if(selectedType.includes('ground')){
+            pokemonImg.setAttribute('style', "background-color: brown;")
+        }else if(selectedType.includes('ghost')){
+            pokemonImg.setAttribute('style', "background-color: lightslategray;")
+        }else if(selectedType.includes('fairy')){
+            pokemonImg.setAttribute('style', "background-color: lightpink;")
+        }else if(selectedType.includes('steel')){
+            pokemonImg.setAttribute('style', "background-color: darkgray;")
+        }else if(selectedType.includes('electric')){
+            pokemonImg.setAttribute('style', "background-color: rgb(239, 239, 109);")
+        }
+
         form.reset()
         })
     })
